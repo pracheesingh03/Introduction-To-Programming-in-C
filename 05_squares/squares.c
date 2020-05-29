689ee9eeab582d01c84c33c1e62636a282bf8f25
Coursera Learner <nobody@nowhere.nul>
2020-05-29 20:14:51 +0000

    Did assigment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,6 +9,8 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
+  if ((coord>= offset) && (coord< (offset+ size)))
+    return 1;
   return 0;
 }
 
@@ -19,9 +21,16 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
+  if((coord== offset) || (coord==(offset+ size)))
+    return 1;
   return 0;
 }
-
+int max(int a, int b, int c){
+  if(a>= (b+c))
+    return a;
+  return (b+c);
+}
+  
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
 
@@ -31,6 +40,22 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
 
     //count from 0 to w. Call the number you count with x
 
+  int w= max(size1, x_offset, size2);
+  int h= max(size1, y_offset, size2);
+    for (int y=0; y<h; y++){
+      for(int x=0; x<w; x++){
+	if(( (isInRange(x, x_offset, size2)) &&( (isAtBorder(y, y_offset, (size2- 1))))) || ( (isInRange(y, y_offset, size2)) && ( (isAtBorder(x, x_offset, (size2- 1)))))){
+	  printf("*");
+	}
+	else if(((x<size1) && ((y==0) || (y==(size1-1)))) || ( (y<size1) && ((x==0) || (x==(size1-1))))){
+	    printf("#");
+	}
+	  else printf(" ");
+	
+      }
+      printf("\n");
+    }
+}
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -50,4 +75,4 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //when you finish counting x from 0 to w, 
     //print a newline
 
-}
+
