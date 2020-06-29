82dd786e7220295a88d71bc8a976843a566dc538
Coursera Learner <nobody@nowhere.nul>
2020-06-29 18:17:34 +0000

    did assignment 23

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -5,7 +5,6 @@ unsigned power(unsigned x, unsigned y)
   if(x==0 && y==0) return 1;
   else if(x==0 && y!=0) return 0;
   else if(x!=0 && y==0) return 1;
-  else if(x!=0 && y<0) return 0;
   else return x*power(x,y-1);
  
 }
