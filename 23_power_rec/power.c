454bef1e9528c35252526bc470393dc1418b0f67
Coursera Learner <nobody@nowhere.nul>
2020-06-29 18:06:50 +0000

    did assignment 23

---
+++ 23_power_rec/power.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+unsigned power(unsigned x, unsigned y)
+{
+  if(x==0 && y==0) return 1;
+  else if(x==0 && y!=0) return 0;
+  else if(x!=0 && y==0) return 1;
+  else {
+    while(y!=0)
+      return x*power(x,y-1);
+  }
+}
