7a447b620ba6faa4282eeae9785b652f329f30e7
Coursera Learner <nobody@nowhere.nul>
2020-06-29 17:27:53 +0000

    did assignment 22

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,48 @@
+#include<stdlib.h>
+#include<stdio.h>
+unsigned power(unsigned x, unsigned y);
+
+int main()
+{
+  unsigned x1=0;
+  unsigned y1=0;
+  if(power(x1,y1)!=1)
+    exit(EXIT_FAILURE);
+
+  unsigned x2=0;
+  unsigned y2=4;
+  if(power(x2,y2)!=0)
+    exit(EXIT_FAILURE);
+
+  unsigned x3=0;
+  unsigned y3=-2;
+  if(power(x3,y3)!=0)
+    exit(EXIT_FAILURE);
+
+  unsigned x4=3;
+  unsigned y4=3;
+  if(power(x4,y4)!=27)
+    exit(EXIT_FAILURE);
+
+  unsigned x5=-2;
+  unsigned y5=2;
+  if(power(x5,y5)!=4)
+    exit(EXIT_FAILURE);
+
+  unsigned x6=3;
+  unsigned y6=0;
+  if(power(x6,y6)!=1)
+    exit(EXIT_FAILURE);
+
+  unsigned x7=-3;
+  unsigned y7=0;
+  if(power(x7,y7)!=1)
+    exit(EXIT_FAILURE);
+
+  unsigned x8=2;
+  unsigned y8=-2;
+  if(power(x8,y8)!=0.25)
+    exit(EXIT_FAILURE);
+
+  exit(EXIT_SUCCESS);
+}
