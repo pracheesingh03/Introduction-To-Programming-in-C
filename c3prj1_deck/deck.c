3423453ded3de574a5cc524b2d319a461cb85890
Coursera Learner <nobody@nowhere.nul>
2020-07-04 14:44:11 +0000

    did c3prj1_deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,39 +2,55 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
-  card_t **card= (*hand).cards;
-  for (int i=0;i<(*hand).n_cards;i++){
-    print_card(**card);
-    card++;
-  }
+
+int com (card_t c1,card_t c2){
+  if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
+  return 0;
 
 }
 
+void print_hand(deck_t * hand){
+  card_t ** card =hand -> cards ;
+  card_t  card1;
+  for (size_t i=0 ;i<(hand -> n_cards );i++){
+    card1=**(card +i);
+    print_card(card1);
+  }
+}
+
 int deck_contains(deck_t * d, card_t c) {
-  card_t **card= (*d).cards;
-  for(int i=0; i<(*d).n_cards; i++)
-    {
-      if((value_letter(**card)==c.value)&&(suit_letter(**card)==c.suit))
-      return 1;
-    card++;
-    }
+  card_t ** card =d -> cards ;
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    if (com(**(card+i),c)) return 1;
+  }
+
   return 0;
 }
 
 void shuffle(deck_t * d){
-  card_t **card=(*d).cards;
-  card_t *temp;
-  for(size_t i=0; i<(*d).n_cards/2; i++)
-    {
-      int y =random()%(*d).n_cards;
-      temp= card[i];
-      card[i]= card[y];
-      card[y]=temp;
-    }
+  card_t ** card =d -> cards ;
+  card_t * temp;
+  size_t n=d ->n_cards;
+  int randarry;
+  for (size_t i=0 ;i< n/2 ;i++){
+    randarry= random()%(n-i)+i;
+    temp=card[i];
+    card[i]=card[randarry];
+    card[randarry]=temp;
+  }
 }
 
 void assert_full_deck(deck_t * d) {
-  card_t **card= (*d).cards;
-  assert_card_valid(**card);
+  card_t ** card =d -> cards ;
+  card_t c;
+  int count;
+
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    c=**(card+i);
+    count=0;
+    for (size_t j=0 ;j< d -> n_cards ;j++){
+      if(com(**(card+j), c)) count ++;
+    }
+    assert(count ==1);
+  }
 }
