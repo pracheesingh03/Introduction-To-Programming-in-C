73773382a3e58fe8d381fc08ce1b99eff6ae7bf6
Coursera Learner <nobody@nowhere.nul>
2020-07-15 11:58:43 +0000

    did assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -30,13 +30,18 @@ int decrypt(FILE *f)
       return EXIT_FAILURE;
     }
   int key;
+  /*
   key= ans - 'e';
   if(key<0)
     {
       key= key*(-1);
       key = 26- key;
     }
-  
+  */
+  key = ans- 'a';
+  if (key>0)
+      key = (key+22)%26;
+  else key = key + (((-key)+22)%26);
   return key;
 }
 int main(int argc, char ** argv)
