011b00431ffc6ee85e6b20a086259d45aa20b5c6
Coursera Learner <nobody@nowhere.nul>
2020-07-17 05:22:28 +0000

    did assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -7,7 +7,30 @@ int decrypt(FILE *f)
   int c;
   int f1;
   int ans;
-  int finalf=0;
+  //int finalf=0;
+  int arr[26];
+  for (int i=0; i<26; i++)
+    {
+      arr[i]=0;
+    }
+  while((c=fgetc(f))!=EOF)
+    {
+      if(isalpha(c))
+	{
+	  c=tolower(c);
+	  arr[c-a]++;
+	}
+    }
+  int max=0;
+  for(int j=0; j<26; j++)
+    {
+      if(arr[j]>max)
+	{
+	max= arr[j];
+	f1=j;
+    }
+      key= f1-4;//4 is the index of e
+  /*
   for(char a='a'; a<='z'; a++)
     {
       f1=0;
@@ -35,17 +58,9 @@ int decrypt(FILE *f)
   char e= 'e';
   int ascii= (int)e;
   key= ans - ascii;
-  if(key<0)
-    {
-      key= key*(-1);
-      key = 26- key;
-    }
-    /*
-  key = ans- 'a';
-  if (key>0)
-      key = (key+22)%26;
-  else key= 26+key;
   */
+  if(key<0)
+    key = key + 26;
   return key;
 }
 int main(int argc, char ** argv)
