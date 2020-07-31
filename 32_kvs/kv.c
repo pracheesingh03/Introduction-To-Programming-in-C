9f5e800b489df38122a90b5c30906f64f5ee9ffe
Coursera Learner <nobody@nowhere.nul>
2020-07-31 08:09:01 +0000

    did assignment 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -56,7 +56,7 @@ void freeKVs(kvarray_t * pairs) {
   //WRITE ME
   for(int i=0; i<(*pairs).length; i++)
     {
-      free(pairs -> arr -> key);
+      free(pairs -> arr[i] -> key);
       free ((*pairs).arr[i]);
     }
   free((*pairs).arr);
